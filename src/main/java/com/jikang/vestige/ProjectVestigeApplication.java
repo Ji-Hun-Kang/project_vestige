2a1b276e9071a5939723f052581d7bc4e6a777d9
JIHUN KANG <jikang9115@gmail.com>
2021-07-23 14:25:12 +0900

    환경세팅 완료

---
+++ src/main/java/com/jikang/vestige/ProjectVestigeApplication.java
@@ -0,0 +1,13 @@
+package com.jikang.vestige;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectVestigeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectVestigeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jikang/vestige/ProjectVestigeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jikang.vestige;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectVestigeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
