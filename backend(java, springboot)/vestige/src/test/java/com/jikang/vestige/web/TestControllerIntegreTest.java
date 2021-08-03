306ac532954d74f4cc4b7f8d6a907dc382de8ab1
Ji-Hun-Kang <jikang9115@gmail.com>
2021-08-03 14:14:05 +0900

    유닛테스트 진행 및 테스트 커밋

--- backend(java, springboot)/vestige/src/test/java/com/jikang/vestige/web/TestControllerIntegreTest.java
+++ backend(java, springboot)/vestige/src/test/java/com/jikang/vestige/web/TestControllerIntegreTest.java
@@ -1,5 +1,25 @@
 package com.jikang.vestige.web;
-// 통합테스트(모든 Bean들을 똑같이 IoC 올리고 테스트하는것)
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * 통합테스트(모든 Bean들을 똑같이 IoC 올리고 테스트하는것)
+ * WebEnvironment.MOCK = 실제 톰켓을 올리를게 아니라, 다른 톰켓으로 테스트한다.
+ * WebEnvironment.RANDOM_POR = 실제 톰켓으로 테스트한다.
+ * @AutoConfigureMockMvc MockMvc를 IoC에 등록해준다.
+ * @Transactional은 각각의 테스트함수가 종료될 때마다 트랜잭션을 rollback해주는 어노테이션이다.
+ */
+@Transactional
+@AutoConfigureMockMvc
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 public class TestControllerIntegreTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
 
 }
